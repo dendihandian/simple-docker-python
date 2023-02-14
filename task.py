a3069b2efe3526c5432f2818b001e9f644760045
Dendi Handian <dendi.handian@tribunnews.com>
2023-02-14 22:36:02 +0700

    using logging streamhandler but still no output to `docker logs`

--- task.py
+++ task.py
@@ -1,3 +1,19 @@
 from datetime import datetime
+import logging
 
-print(f'current datetime: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+def get_module_logger(mod_name):
+    """
+    To use this, do logger = get_module_logger(__name__)
+    """
+    logger = logging.getLogger(mod_name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+if __name__ == "__main__":
+    get_module_logger(__name__).debug(f'current datetime: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
