04440801bc33364e917f1fbd4734aa6b2c708bd3
Dendi Handian <dendi.handian@tribunnews.com>
2023-02-14 23:04:41 +0700

    change to sys.stdout.write but still no output to docker logs

--- task.py
+++ task.py
@@ -1,19 +1,10 @@
 from datetime import datetime
-import logging
-
-
-def get_module_logger(mod_name):
-    """
-    To use this, do logger = get_module_logger(__name__)
-    """
-    logger = logging.getLogger(mod_name)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
-
+import sys
 
 if __name__ == "__main__":
-    get_module_logger(__name__).debug(f'current datetime: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    level = 'INFO'
+    message = 'Hello, this is an info.'
+
+    sys.stdout.write(f"[{timestamp}] {{__name__}} {level} - {message}")
