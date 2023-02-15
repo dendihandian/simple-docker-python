697db25455427aa6b24b879b788e106af9e2e869
Dendi Handian <dendi.handian@tribunnews.com>
2023-02-16 00:18:42 +0700

    enable all level logging

--- task.py
+++ task.py
@@ -4,13 +4,16 @@ import logging
 
 if __name__ == "__main__":
 
+    # NOTE: by default, logging only print warning, error, critical. we need to set level to DEBUG to see all logging in the logs.
+    logging.basicConfig(filename='service.log', encoding='utf-8', level=logging.DEBUG)
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = 'Hello, this is an info.'
 
-    sys.stdout.write(f"[{timestamp}] task.py sys.stdout.write - {message} (sys.stdout.write)\n")
+    sys.stdout.write(f"[{timestamp}] task.py sys.stdout.write - {message} (sys.stdout.write)")
     print(f"[{timestamp}] task.py PRINT - {message} (print)")
-    logging.debug(f"[{timestamp}] task.py DEBUG - {message} (logging.debug)\n")
-    logging.info(f"[{timestamp}] task.py INFO - {message} (logging.info)\n")
+    logging.debug(f"[{timestamp}] task.py DEBUG - {message} (logging.debug)")
+    logging.info(f"[{timestamp}] task.py INFO - {message} (logging.info)")
     logging.warning(f"[{timestamp}] task.py WARNING - {message} (logging.warning)")
     logging.error(f"[{timestamp}] task.py ERROR - {message} (logging.error)")
     logging.critical(f"[{timestamp}] task.py CRITICAL - {message} (logging.critical)")
